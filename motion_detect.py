6a660e73b5afeb8fb89bb7d81516aa6449f9a168
kaykoe <karolkoenigg@gmail.com>
2025-04-27 01:30:07 +0200

    feat: improve logging functionality, fix fluke detection

--- motion_detect.py
+++ motion_detect.py
@@ -3,12 +3,13 @@ import os
 import signal
 import sys
 import time
+from logging.handlers import TimedRotatingFileHandler
 
 import numpy as np
 from picamera2 import Picamera2
 from picamera2.encoders import H264Encoder, Quality
 from picamera2.outputs import FileOutput
-from systemd import journal
+from systemd.journal import JournalHandler
 
 # TODO: more resilient false positive detection
 # TODO: test out opencv
@@ -18,32 +19,60 @@ from systemd import journal
 
 def remove_fluke_recordings():
     if motion_frames < 10:
-        filename = encoder.output().fileoutput()
+        filename = encoder.output.fileoutput
         os.remove(filename)
         logger.info("Less than 10 motion frames, deleting Recording")
 
 
 # Shutdown gracefully
-def handler(signum, frame):
+def handle_signal(signum, frame):
     logger.info(f"Received signal {signal.strsignal(signum)}, shutting down")
     if encoding:
         picam2.stop_encoder()
-        logger.info("Stopping Recording")
+        logger.info("Stopping Recording, motion_frames={motion_frames}")
         remove_fluke_recordings()
     sys.exit(0)
 
 
-signal.signal(signal.SIGINT, handler)
-signal.signal(signal.SIGTERM, handler)
+signal.signal(signal.SIGINT, handle_signal)
+signal.signal(signal.SIGTERM, handle_signal)
+
 
 # Configure logging
-logging.basicConfig(
-    format="%(levelname)s - %(message)s",
-)
 logger = logging.getLogger(__name__)
 logger.propagate = False
-logger.setLevel(logging.INFO)
-logger.addHandler(journal.JournalHandler())
+logger.setLevel(logging.DEBUG)
+
+if not os.path.isdir("/home/solvrocam/hardware-solvro-bot-office-cam/logs"):
+    os.mkdir("/home/solvrocam/hardware-solvro-bot-office-cam/logs")
+
+file_handler = TimedRotatingFileHandler(
+    "/home/solvrocam/hardware-solvro-bot-office-cam/logs/log",
+    when="D",
+    backupCount=7,
+)
+file_handler.setLevel(logging.DEBUG)
+file_handler.setFormatter(
+    logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+)
+logger.addHandler(file_handler)
+
+journal_handler = JournalHandler()
+journal_handler.setLevel(logging.INFO)
+journal_handler.setFormatter(logging.Formatter("%(levelname)s - %(message)s"))
+logger.addHandler(journal_handler)
+
+
+def handle_exception(exc_type, exc_value, exc_traceback):
+    if issubclass(exc_type, KeyboardInterrupt):
+        sys.__excepthook__(exc_type, exc_value, exc_traceback)
+        return
+
+    logger.critical("Uncaught exception", exc_info=(exc_type, exc_value, exc_traceback))
+
+
+sys.excepthook = handle_exception
+
 
 lsize = (320, 240)
 picam2 = Picamera2()
