29794cd6bf8f8589cdebc85c2489b71e88c799a6
kaykoe <karolkoenigg@gmail.com>
2025-05-23 12:08:26 +0200

    refactor: extract communication with core to a separate file

--- src/solvro_cam/main.py
+++ src/solvro_cam/main.py
@@ -1,15 +1,12 @@
-import datetime
 import logging
-import os
 import sys
-from urllib.parse import urljoin
 
 import cv2
 from dotenv import load_dotenv
 from picamera2 import Picamera2  # pyright: ignore[reportMissingImports]
-from requests import post
 
 from solvro_cam.logs import setup_logging
+from solvro_cam.core import ping
 from solvro_cam.person_trackers.yolo_bytetracker import YOLOByteTracker
 
 # TODO: break this file up into smaller modules
@@ -30,43 +27,7 @@ def handle_exception(exc_type, exc_value, exc_traceback):
 sys.excepthook = handle_exception
 
 
-def debounce(func, time=datetime.timedelta(seconds=15)):
-    last_call = {"time": datetime.datetime.min}
-
-    def wrapper(*args, **kwargs):
-        if datetime.datetime.now() - last_call["time"] >= time:
-            last_call["time"] = datetime.datetime.now()
-            return func(*args, **kwargs)
-
-    return wrapper
-
-
 load_dotenv()
-core_url = os.getenv("CORE_URL")
-
-
-@debounce
-def ping(count, image):
-    # TODO: finish integrating with core
-    if core_url is not None:
-        if count > 0:
-            post(
-                urljoin(core_url, "office/camera"),
-                data={
-                    "timestamp": datetime.datetime.now(datetime.timezone.utc),
-                    "count": count,
-                },
-                files={"file": image},
-            )
-        else:
-            post(
-                urljoin(core_url, "office/camera"),
-                json={
-                    "timestamp": datetime.datetime.now(datetime.timezone.utc),
-                    "count": count,
-                },
-            )
-
 
 main_size = (4608, 2592)
 downscaled_size = (576, 320)
