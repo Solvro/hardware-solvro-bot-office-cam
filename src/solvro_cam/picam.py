4493d4b64129521fb87a93f7a1a06a7cfd56a89c
kaykoe <karolkoenigg@gmail.com>
2025-05-23 13:16:54 +0200

    refactor: extract camera setup to separate file and make the program into an executable pipx typer cli

---
+++ src/solvro_cam/picam.py
@@ -0,0 +1,19 @@
+from picamera2 import Picamera2  # pyright: ignore[reportMissingImports]
+
+main_size = (4608, 2592)
+main_format = "YUV420"
+framerate = 10
+
+
+def setup_camera() -> Picamera2:
+    picam2 = Picamera2()
+    video_config = picam2.create_video_configuration(
+        main={"size": main_size, "format": main_format},
+        display=None,
+        buffer_count=5,
+        controls={"FrameRate": framerate},
+    )
+
+    picam2.configure(video_config)
+    picam2.start()
+    return picam2
