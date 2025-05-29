c52b21b62733c3e04161df7ee03baed97855a12f
kaykoe <karolkoenigg@gmail.com>
2025-05-29 23:33:31 +0200

    fix: minimize downtime when reinstalling app

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-uv tool install --reinstall --no-managed-python .
+uv tool install --reinstall --no-managed-python --compile-bytecode .
 sed -i 's/\(include-system-site-packages = \)false/\1true/' ~/.local/share/uv/tools/hardware-solvro-bot-office-cam/pyvenv.cfg
-sudo systemctl stop motion_detect.service
 sudo cp ./motion_detect.service /etc/systemd/system/ &&
 	sudo systemctl daemon-reload &&
 	sudo systemctl restart motion_detect.service
